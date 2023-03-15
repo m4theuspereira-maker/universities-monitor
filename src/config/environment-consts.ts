55b0e2309e89c271665f649e57bc5d5492a4d886
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-03-15 13:53:47 -0300

    refactor: add configs for env vars

--- src/config/environment-consts.ts
+++ src/config/environment-consts.ts
@@ -1,1 +1,8 @@
+import { resolve } from "path";
+import { config } from "dotenv";
+
+config({ path: resolve(__dirname, "../../.env") });
+
 export const PORT = process.env.PORT || 3000;
+export const DATABASE_URL = process.env.DATABASE_URL;
+export const DEFAULT_PAGE_LIMIT = 20
