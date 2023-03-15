1ac2844fb61f8b73d556a5ee3bd222b7656e5db6
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-03-15 20:55:08 -0300

    feat: add open API

--- src/config/environment-consts.ts
+++ src/config/environment-consts.ts
@@ -1,8 +1,11 @@
 import { resolve } from "path";
 import { config } from "dotenv";
+import swaggerDocs from "./swagger.json";
+
 
 config({ path: resolve(__dirname, "../../.env") });
 
 export const PORT = process.env.PORT || 3000;
 export const DATABASE_URL = process.env.DATABASE_URL;
 export const DEFAULT_PAGE_LIMIT = 20
+export const SWAGGER_DOCS = swaggerDocs
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import cors from "cors";
-import { PORT } from "./config/environment-consts";
+import { PORT, SWAGGER_DOCS } from "./config/environment-consts";
 import { routes } from "./routes";
 import { connectToDatabase } from "./infra/database/mongodb/connection/connection";
+import swaggerUi from "swagger-ui-express";
+
 const app = express();
 app.use(cors());
 app.use(routes);
 
 const server = app.listen(PORT, () => {
+  app.use("/api", swaggerUi.serve, swaggerUi.setup(SWAGGER_DOCS));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   connectToDatabase();
