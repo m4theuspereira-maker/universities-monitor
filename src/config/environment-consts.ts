a76d3e05bd2f0b2c433f754f439269ea4794bf0c
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-03-16 23:36:01 -0300

    chore: add new const

--- src/config/environment-consts.ts
+++ src/config/environment-consts.ts
@@ -9,3 +9,13 @@ export const DATABASE_URL = process.env.DATABASE_URL;
 export const DEFAULT_PAGE_LIMIT = 20;
 export const SWAGGER_DOCS = swaggerDocs;
 export const APP_SECRET = process.env.APP_SECRET;
+export const COUNTRIES_UNIVERSITIES_TO_BE_TAKEN = [
+  "argentina",
+  "brasil",
+  "chile",
+  "colombia",
+  "paraguai",
+  "peru",
+  "suriname",
+  "uruguay"
+];
